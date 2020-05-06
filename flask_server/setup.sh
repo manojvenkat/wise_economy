c5da83ef7baa3ae1d5bfb89a78cec7e6c526fccf
Manoj Tutika <manojt@yelp.com>
2020-05-06 23:14:29 +0200

    Requirements.txt freeze.

--- flask_server/setup.sh
+++
@@ -1,5 +0,0 @@
-pip3 install virtualenv
-virtualenv venv
-source venv/bin/activate
-pip3 install flask flask-graphql flask-migrate flask-sqlalchemy graphene graphene-sqlalchemy
-python3 uwsgi.py
--- setup.sh
+++ setup.sh
@@ -2,4 +2,4 @@ pip3 install virtualenv
 virtualenv venv
 source venv/bin/activate
 pip3 install -r requirements.txt
-
+python3 -m "flask_server.uwsgi"
